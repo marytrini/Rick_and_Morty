ee5d603dbbe1d30efcd481684ce7eeb620510064
Maria Salazar <mariatrinidadsalazarr@gmail.com>
2024-08-01 01:05:43 -0400

    feat(structure): Add components, episodes, characters, favorites

---
+++ src/app/characters/page.tsx
@@ -0,0 +1,10 @@
+
+const Characters = () => {
+  return (
+    <div>
+        <h1>All Characters...</h1>
+    </div>
+  )
+}
+
+export default Characters
---
+++ src/app/episodes/page.tsx
@@ -0,0 +1,11 @@
+
+const Episodes = () => {
+  return (
+    <div>
+        <h1>All Episodes...</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos eveniet quae repellat nulla odio assumenda! Dolorum nulla itaque nobis facilis fuga unde explicabo, suscipit fugiat? Officia maiores recusandae cupiditate! Fuga.</p>
+    </div>
+  )
+}
+
+export default Episodes
